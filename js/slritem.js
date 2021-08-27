bba5229d2059cd1bd456fd53d24c4e53f552bdf5
sabil62 <shresthasabil62@gmail.com>
2021-08-27 19:23:49 +0545

    reversion

--- js/slritem.js
+++
@@ -1,8 +0,0 @@
-//somente verifica se o tipo da gramatica é SLR
-function Item(rule, dotIndex)
-{
-	extend(this, new BasicItem(rule, dotIndex));
-	this.lookAheads = rule.grammar.follows[rule.nonterminal];
-}
-
-Item.prototype.grammarType = 'SLR';
